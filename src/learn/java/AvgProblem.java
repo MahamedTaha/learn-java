4d6a12da5449e5e9d6b3998e6655d073d9b07bb5
Mohamed Taha <thm614372@gmail.com>
2023-08-01 10:22:29 +0300

    Solve AVG problem

--- src/learn/java/AvgProblem.java
+++ src/learn/java/AvgProblem.java
@@ -4,10 +4,28 @@
  */
 package learn.java;
 
+import java.util.Scanner;
+
 /**
  *
  * @author khale
  */
 public class AvgProblem {
-    
+    public static void main(String[] args) {
+        Scanner M = new Scanner(System.in);
+
+        System.out.print("Enter the number of numbers you want to average : ");
+        int count = M.nextInt();
+
+        int sum = 0;
+        for (int a = 1; a <= count; a++) {
+
+            System.out.print("enter num " + a + " : ");
+            int num = M.nextInt();
+
+            sum += num;
+
+        }
+        System.out.println("The Avarge : " + sum / count);
+    }
 }
