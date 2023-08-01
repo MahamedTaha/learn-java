79e25dc74aa75082a0080ccebbcad367d65e9503
Mohamed Taha <thm614372@gmail.com>
2023-08-01 10:22:56 +0300

    Solve AVG problem

--- src/learn/java/LearnJava.java
+++ src/learn/java/LearnJava.java
@@ -16,21 +16,7 @@ public class LearnJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner M = new Scanner(System.in);
-
-        System.out.print("Enter the number of numbers you want to average : ");
-        int count = M.nextInt();
-
-        int sum = 0;
-        for (int a = 1; a <= count; a++) {
-
-            System.out.print("enter num " + a + " : ");
-            int num = M.nextInt();
-
-            sum += num;
-
-        }
-        System.out.println("The Avarge : " + sum / count);
+        
 
     }
 
